Updated the 1 loop calculation to fix some bugs

NewData/SRC/MXNET_GPU_CNN_SuperVised/program.py
@@ -6,11 +6,15 @@ from mxnet import nd, autograd, gluon
 import os
 import lzma
 
-ctx = mx.cpu()
+ctx = mx.gpu()
 num_outputs = 2
 batch_size = 200
 width = 40
 image_size = width*width
+epochs = 10
+num_fc = 32
+smoothing_constant = 0.01
+PARAM_NAME = "./cnn_top_tagger_pars"
 
 def READ_XZ (filename):
     file = lzma.LZMAFile(filename)
@@ -18,10 +22,7 @@ def READ_XZ (filename):
     type_array = np.frombuffer(type_bytes,dtype='float32')
     return type_array
 
-
-num_fc = 32
 net = gluon.nn.HybridSequential()
-smoothing_constant = 0.01
 with net.name_scope():
     net.add(gluon.nn.Conv2D(channels=50, kernel_size=10, activation='relu'))
     net.add(gluon.nn.MaxPool2D(pool_size=2, strides=2))
@@ -34,45 +35,76 @@ with net.name_scope():
     net.add(gluon.nn.Dense(num_outputs))
 net.hybridize()
 net.collect_params().initialize(mx.init.Xavier(magnitude=2.24), ctx=ctx)
-
 softmax_cross_entropy = gluon.loss.SoftmaxCrossEntropyLoss()
 trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': 0.001})
 
-epochs = 10
+def EvalOnFile(name_image,name_label):
 
-def evaluate_accuracy(data_iterator):
+    input_data = nd.array(READ_XZ(name_image).reshape(-1,batch_size,1,width,width))
+    input_data_ctx = input_data.as_in_context(ctx)
+
+    input_label = nd.array(READ_XZ(name_label).reshape(-1,batch_size))
+    input_label_ctx = input_label.as_in_context(ctx)
+
+    batches = input_data.shape[0]
+#    batches = 5
+
+    print(input_data.shape[0])
+    print(input_label.shape[0])
+
     acc = mx.metric.Accuracy()
-    for i, (data, label) in enumerate(data_iterator):
-        data = data.as_in_context(ctx)
-        label = label.as_in_context(ctx)
-        output = net(data)
+    for i in range(batches):
+        output = net(input_data_ctx[i])
         predictions = nd.argmax(output, axis=1)
-        prednew = nd.reshape(predictions,label.shape)
-        acc.update(preds=prednew, labels=label)
+        prednew = nd.reshape(predictions,input_label_ctx[i].shape)
+        acc.update(preds=prednew, labels=input_label_ctx[i])
     return acc.get()[1]
 
-def TrainOnFile(name_image,name_label,name_val_image,name_val_label):
-    input_data  = nd.array(READ_XZ(name_image).reshape(-1,1,width,width))
-    input_label = nd.array(READ_XZ(name_label).reshape(-1))
-    dataset = mx.gluon.data.dataset.ArrayDataset(input_data, input_label)
-    train_data = mx.gluon.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
-    print(input_data.shape)
-    print(input_label.shape)
+def TrainOnFile(name_image,name_label,name_image_test,name_label_test):
+
+    input_data = nd.array(READ_XZ(name_image).reshape(-1,batch_size,1,width,width))
+    input_data_ctx = input_data.as_in_context(ctx)
+
+    input_label = nd.array(READ_XZ(name_label).reshape(-1,batch_size))
+    input_label_ctx = input_label.as_in_context(ctx)
+
+    batches = input_data.shape[0]
+#    batches = 5
+
+    print(input_data.shape[0])
+    print(input_label.shape[0])
+
     for e in range(epochs):
         cumulative_loss = 0
-        for i, (data, label) in enumerate(train_data):
-            data = data.as_in_context(ctx)
-            label = label.as_in_context(ctx)
+        for i in range(batches):
             with autograd.record():
-                output = net(data)
-                loss = softmax_cross_entropy(output, label)
+                output = net(input_data_ctx[i])
+                loss = softmax_cross_entropy(output, input_label_ctx[i])
             loss.backward()
-            trainer.step(data.shape[0])
+            trainer.step(batch_size)
             inst_loss = nd.sum(loss).asscalar()
+            if((i%100)==0):
+                print(inst_loss)
             cumulative_loss += inst_loss
             curr_loss = inst_loss
-            moving_loss = (curr_loss if ((i == 0) and (e == 0))
-                else (1 - smoothing_constant) * moving_loss + smoothing_constant * curr_loss)
+            moving_loss = (
+                curr_loss if ((i == 0) and (e == 0))
+                else (1 - smoothing_constant) * moving_loss + smoothing_constant * curr_loss
+            )
         print(cumulative_loss)
-        train_accuracy = evaluate_accuracy(train_data)
-        print("Epoch %s. Loss: %s, Train_acc %s" % (e, moving_loss, train_accuracy))
+        net.save_parameters(PARAM_NAME)
+        train_accuracy = EvalOnFile(name_image,name_label)
+        test_accuracy = EvalOnFile(name_image_test,name_label_test)
+        print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, train_accuracy))
+
+TrainOnFile("./TRAIN/0/image.xz","./TRAIN/0/label.xz","./TEST/0/image.xz","./TEST/0/label.xz")
+TrainOnFile("./TRAIN/1/image.xz","./TRAIN/1/label.xz","./TEST/1/image.xz","./TEST/1/label.xz")
+TrainOnFile("./TRAIN/2/image.xz","./TRAIN/2/label.xz","./TEST/2/image.xz","./TEST/2/label.xz")
+TrainOnFile("./TRAIN/3/image.xz","./TRAIN/3/label.xz","./TEST/3/image.xz","./TEST/3/label.xz")
+TrainOnFile("./TRAIN/4/image.xz","./TRAIN/4/label.xz","./TEST/4/image.xz","./TEST/4/label.xz")
+TrainOnFile("./TRAIN/5/image.xz","./TRAIN/5/label.xz","./TEST/5/image.xz","./TEST/5/label.xz")
+TrainOnFile("./TRAIN/6/image.xz","./TRAIN/6/label.xz","./TEST/6/image.xz","./TEST/6/label.xz")
+TrainOnFile("./TRAIN/7/image.xz","./TRAIN/7/label.xz","./TEST/7/image.xz","./TEST/7/label.xz")
+#test_accuracy = evaluate_accuracy(test_data, net)
+#train_accuracy = evaluate_accuracy(train_data, net)
+#print("Epoch %s. Loss: %s, Train_acc %s, Test_acc %s" % (e, moving_loss, train_accuracy, test_accuracy))