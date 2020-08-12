0a0ca17e692aa255a37ea0c3af3a83826a466631
aravind <aravindhv10@gmail.com>
2020-08-12 05:01:38 +0000

    added comparison functions to read_write_functions

--- LatestHeaders/CPPFileIO/Read_Show_Functions.hh
+++ LatestHeaders/CPPFileIO/Read_Show_Functions.hh
@@ -1,5 +1,76 @@
 namespace Read_Show_Functions {
 
+#define COMPARE_INT(intype)                  \
+	inline int                           \
+	Compare (                            \
+		intype const a               \
+		, intype const b             \
+	) {                                  \
+		return                       \
+			  ( -1 * (a  < b ) ) \
+			+ (  0 * (a == b ) ) \
+			+ (  1 * (a  > b ) ) \
+		;                            \
+	}                                    //
+	COMPARE_INT(char)
+	COMPARE_INT(unsigned char)
+	COMPARE_INT(int)
+	COMPARE_INT(unsigned int)
+	COMPARE_INT(long)
+	COMPARE_INT(size_t)
+	COMPARE_INT(float)
+	COMPARE_INT(double)
+#undef COMPARE_INT
+
+	inline int
+	Compare (
+		char const * S1
+		, char const * S2
+	) {
+		int const val = strcmp(S1,S2);
+		return Compare(val,0);
+	}
+
+	template <size_t m, size_t n>
+	inline int
+	Compare(
+		Tensors::Array::ND_ARRAY <m,char> & S1
+		, Tensors::Array::ND_ARRAY <n,char> & S2
+	) {
+		return
+			Compare(
+				S1.GET_DATA()
+				, S2.GET_DATA()
+			)
+		; //
+	}
+
+	template <size_t n>
+	inline int
+	Compare(
+		Tensors::Array::ND_ARRAY <n,char> & S1
+		, std::string const & S2
+	) {
+		return Compare(S1.GET_DATA(),S2.c_str());
+	}
+	
+	template <size_t n>
+	inline int
+	Compare(
+		std::string const & S1
+		, Tensors::Array::ND_ARRAY <n,char> & S2
+	) {
+		return Compare(S2,S1) * -1 ;
+	}
+
+	inline int
+	Compare(
+		std::string const & S1
+		, std::string const & S2
+	) {
+		return Compare(S1.c_str(),S2.c_str()) ;
+	}
+
 	static inline void
 	Show_Label
 	(	char const * c
