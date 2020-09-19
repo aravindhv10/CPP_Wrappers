b953393e2e5ef3976c0ff19358497f06f43973bb
aravind <aravindhv10@gmail.com>
2020-09-19 14:08:32 +0000

    Working grad neural network rewrite

---
+++ LatestHeaders/new/src/EXAMPLE_NN/LOG/main.hh
@@ -0,0 +1,13 @@
+﻿//BASIC::Net::CREATE();
+//BASIC::Net::TRAIN();
+
+NN_GRAD_LOG::MainNet::CREATE();
+NN_GRAD_LOG::MainNet::TRAIN();
+//for(int i=0;i<4;i++){
+//NN_GRAD_LOG::MainNet::TRAIN_ZERO();
+//}
+
+//for(int i=0;i<100;i++){
+//NN_GRAD_LOG::MainNet::TRAIN_ZERO();
+//NN_GRAD_LOG::MainNet::TRAIN();
+//}
