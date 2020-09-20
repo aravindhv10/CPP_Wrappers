b8b7cd09813c09b83c022365143b8de3e9460b79
Aravind <aravindhv10@gmail.com>
2020-09-20 15:51:14 +0530

    added inplace normalizer

--- LatestHeaders/new/include/CPPFileIO.dir/Correlation.hh
+++ LatestHeaders/new/include/CPPFileIO.dir/Correlation.hh
@@ -6,15 +6,36 @@
 #define _MACRO_MAIN_TYPE_ Dynamic1DArray <T>
 
 template <typename T>
-_MACRO_MAIN_TYPE_
-GetNormalized (_MACRO_MAIN_TYPE_ const & in) {
-
+inline void
+NormalizeInPlace (_MACRO_MAIN_TYPE_ & in) {
 	T sum[2] = {0,0} ;
 	for(size_t i=0;i<in();i++){
 		sum[0] += in(i) ;
 		sum[1] += std::pow(in(i),2) ;
 	}
+	T const mu = sum[0] / static_cast <T> (in()) ;
+	T const sigma =
+		std::sqrt(
+			( sum[1] / static_cast <T> (in()) )
+			- std::pow(mu,2)
+		)
+	;
+	for(size_t i=0;i<in();i++){
+		in(i) =
+			( in(i) - mu )
+			/ sigma
+		;
+	}
+}
 
+template <typename T>
+_MACRO_MAIN_TYPE_
+GetNormalized (_MACRO_MAIN_TYPE_ const & in) {
+	T sum[2] = {0,0} ;
+	for(size_t i=0;i<in();i++){
+		sum[0] += in(i) ;
+		sum[1] += std::pow(in(i),2) ;
+	}
 	T const mu = sum[0] / static_cast <T> (in()) ;
 	T const sigma =
 		std::sqrt(
