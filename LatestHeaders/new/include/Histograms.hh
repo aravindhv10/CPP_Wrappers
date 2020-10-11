73b431d5dc5252ddfe3f71025558e70b72067158
Aravind <aravindhv10@gmail.com>
2020-10-11 16:27:29 +0530

    Fixed bug in dynamic histogram

--- LatestHeaders/new/include/Histograms.hh
+++ LatestHeaders/new/include/Histograms.hh
@@ -137,9 +137,9 @@ template <typename T = float> class _MACRO_CLASS_NAME_ {
   private:
     static inline size_t constexpr N1() { return 0; }
 
-    inline size_t N2() { return DATA(); }
+    inline size_t N2() const { return DATA(); }
 
-    inline TYPE_DATA N2_Minux_M1() {
+    inline TYPE_DATA N2_Minux_M1() const {
         return static_cast<TYPE_DATA>(N2() - N1());
     }
 
@@ -192,7 +192,7 @@ template <typename T = float> class _MACRO_CLASS_NAME_ {
     _MACRO_CLASS_NAME_(TYPE_DATA const _Begin, TYPE_DATA const _End,
                        size_t const _N)
       : Begin(_Begin), End(_End), DATA(_N) {
-        DATA = 0;
+        for (size_t i = 0; i < DATA(); i++) { DATA(i) = 0; }
     }
 
     ~_MACRO_CLASS_NAME_() {}
@@ -205,94 +205,78 @@ template <typename T = float> class _MACRO_CLASS_NAME_ {
 
 #define _MACRO_CLASS_NAME_ FullHist
 
-template <typename T>
-class _MACRO_CLASS_NAME_ {
+template <typename T> class _MACRO_CLASS_NAME_ {
 
-public:
-	using TYPE_DATA = T ;
-	using TYPE_SELF = _MACRO_CLASS_NAME_ <TYPE_DATA> ;
+  public:
+    using TYPE_DATA = T;
+    using TYPE_SELF = _MACRO_CLASS_NAME_<TYPE_DATA>;
 
-private:
-	std::string const source_name ;
+  private:
+    std::string const source_name;
 
-private:
-	inline std::string dirname () const {
-		std::string ret = source_name + ".dir/" ;
-		mkdir(ret.c_str(),0755);
-		return ret ;
-	}
+  private:
+    inline std::string dirname() const {
+        std::string ret = source_name + ".dir/";
+        mkdir(ret.c_str(), 0755);
+        return ret;
+    }
 
-	inline std::string histname() const {
-		return dirname() + "out.hist.txt" ;
-	}
-	inline std::string dataname() const {
-		return dirname() + "out.data.bin" ;
-	}
-	inline std::string statsname() const {
-		return dirname() + "out.stats.txt" ;
-	}
+    inline std::string histname() const { return dirname() + "out.hist.txt"; }
+    inline std::string dataname() const { return dirname() + "out.data.bin"; }
+    inline std::string statsname() const { return dirname() + "out.stats.txt"; }
 
-private:
-	inline void copy () {
-		CPPFileIO::FullFileReader <TYPE_DATA> reader(source_name) ;
-		CPPFileIO::FileWriter <TYPE_DATA> writer(dataname()) ;
-		for(size_t i=0;i<reader();i++){
-			writer(reader(i));
-		}
-	}
+  private:
+    inline void copy() {
+        CPPFileIO::FullFileReader<TYPE_DATA> reader(source_name);
+        CPPFileIO::FileWriter<TYPE_DATA>     writer(dataname());
+        for (size_t i = 0; i < reader(); i++) { writer(reader(i)); }
+    }
 
-	inline void sort () {
-		CPPFileIO::SortFile <TYPE_DATA> (dataname()) ;
-	}
+    inline void sort() { CPPFileIO::SortFile<TYPE_DATA>(dataname()); }
 
-public:
-	inline void MakeHist (TYPE_DATA const begin, TYPE_DATA const end, size_t const n_bins) {
-		copy();
-		sort();
-		DynamicHist <TYPE_DATA> hist (begin,end,n_bins) ;
-		CPPFileIO::FullFileReader <TYPE_DATA> reader(dataname()) ;
-		for(size_t i=0;i<reader();i++){
-			hist(reader(i));
-		}
-		hist >> histname() ;
-	}
+  public:
+    inline void MakeHist(TYPE_DATA const begin, TYPE_DATA const end,
+                         size_t const n_bins) {
+        copy();
+        sort();
+        DynamicHist<TYPE_DATA>               hist(begin, end, n_bins);
+        CPPFileIO::FullFileReader<TYPE_DATA> reader(dataname());
+        for (size_t i = 0; i < reader(); i++) { hist(reader(i)); }
+        hist >> histname();
+    }
 
-	inline void MakeHist (size_t const n_bins) {
-		copy();
-		sort();
-		CPPFileIO::FullFileReader <TYPE_DATA> reader(dataname()) ;
-		DynamicHist <TYPE_DATA> hist (reader(0),reader(reader()-1),n_bins) ;
-		for(size_t i=0;i<reader();i++){
-			hist(reader(i));
-		}
-		hist >> histname() ;
-	}
+    inline void MakeHist(size_t const n_bins) {
+        copy();
+        sort();
+        CPPFileIO::FullFileReader<TYPE_DATA> reader(dataname());
+        DynamicHist<TYPE_DATA> hist(reader(0), reader(reader() - 1), n_bins);
+        for (size_t i = 0; i < reader(); i++) { hist(reader(i)); }
+        hist >> histname();
+    }
 
-	inline void MakeStats (FILE * f, TYPE_DATA const fraction) {
-		CPPFileIO::FullFileReader <TYPE_DATA> reader(dataname()) ;
-		size_t const limit = reader() - 1 ;
-		size_t const index = static_cast <size_t> ( fraction * static_cast<TYPE_DATA>(limit) );
-		fprintf(f,"%e : %e\n",fraction,reader(index));
-	}
+    inline void MakeStats(FILE *f, TYPE_DATA const fraction) {
+        CPPFileIO::FullFileReader<TYPE_DATA> reader(dataname());
+        size_t const                         limit = reader() - 1;
+        size_t const                         index =
+          static_cast<size_t>(fraction * static_cast<TYPE_DATA>(limit));
+        fprintf(f, "%e : %e\n", fraction, reader(index));
+    }
 
-	inline void MakeStats (std::vector <TYPE_DATA> const & in) {
-		FILE * f = fopen(statsname().c_str(),"w");
-		for(size_t i=0;i<in.size();i++){
-			MakeStats(f,in[i]);
-		}
-		fclose(f);
-	}
+    inline void MakeStats(std::vector<TYPE_DATA> const &in) {
+        FILE *f = fopen(statsname().c_str(), "w");
+        for (size_t i = 0; i < in.size(); i++) { MakeStats(f, in[i]); }
+        fclose(f);
+    }
 
-public:
-	_MACRO_CLASS_NAME_(std::string const _source_name) : source_name(_source_name){
-		copy();
-		sort();
-	}
-	~_MACRO_CLASS_NAME_(){}
-
-} ;
+  public:
+    _MACRO_CLASS_NAME_(std::string const _source_name)
+      : source_name(_source_name) {
+        copy();
+        sort();
+    }
+    ~_MACRO_CLASS_NAME_() {}
+};
 
 #undef _MACRO_CLASS_NAME_
-
 
 #endif
