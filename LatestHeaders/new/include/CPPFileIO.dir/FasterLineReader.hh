f2c3a7d77f17f44e65759790bb8029e2580eddc5
aravind <aravindhv10@gmail.com>
2020-10-13 22:04:30 +0530

    Started boundary class

--- LatestHeaders/new/include/CPPFileIO.dir/FasterLineReader.hh
+++ LatestHeaders/new/include/CPPFileIO.dir/FasterLineReader.hh
@@ -145,4 +145,52 @@ template <char seperator = '\t', char newline = '\n'> class _MACRO_CLASS_NAME_ {
 
 #undef _MACRO_CLASS_NAME_
 
+#define _MACRO_CLASS_NAME_ FileDivider
+
+template <char endline> class _MACRO_CLASS_NAME_ {
+  public:
+    using TYPE_SELF       = _MACRO_CLASS_NAME_<endline>;
+    using TYPE_BOUNDARIES = std::vector<size_t>;
+    using TYPE_READER     = FileArray<char const>;
+
+  private:
+    std::string const FILENAME;
+    TYPE_READER       READER;
+    TYPE_BOUNDARIES   BOUNDARIES;
+    size_t const      LIMIT;
+
+  private:
+    inline void CRAWL(size_t const i) {
+        if (i < (BOUNDARIES.size() - 1)) {
+            size_t const start  = BOUNDARIES[i];
+            size_t const length = BOUNDARIES[i + 1] - BOUNDARIES[i];
+            for (size_t j = start; j < length; j++) {
+					printf("DEBUG LOOP\n");
+                if (READER(j) == endline) {
+						printf("DEBUG TRUE\n");
+                    BOUNDARIES[i] = j;
+                    return;
+                }
+            }
+        }
+    }
+
+  public:
+    inline TYPE_BOUNDARIES const &get_boundaries(size_t const nums = 1) {
+        BOUNDARIES.resize(nums + 1);
+        for (size_t i = 0; i <= nums; i++) {
+            BOUNDARIES[i] = (LIMIT * i) / nums;
+			printf("DEBUG %zu %zu %zu %zu\n",LIMIT,i,nums,BOUNDARIES[i]);
+        }
+        for (size_t i = 1; i < nums; i++) { CRAWL(i); }
+        return BOUNDARIES;
+    }
+
+    _MACRO_CLASS_NAME_(std::string const filename)
+      : FILENAME(filename), READER(FILENAME), LIMIT(READER()) {}
+    ~_MACRO_CLASS_NAME_() {}
+};
+
+#undef _MACRO_CLASS_NAME_
+
 #endif
