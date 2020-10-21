e03f1e12b9ebff1d998919a75ef7c61135e5ae84
Aravind <aravindhv10@gmail.com>
2020-10-21 17:03:19 +0530

    Improved dynamic1dstore and symmetricmatrix

--- LatestHeaders/new/include/CPPFileIO.dir/D1.hh
+++ LatestHeaders/new/include/CPPFileIO.dir/D1.hh
@@ -11,331 +11,188 @@
 
 #define _MACRO_CLASS_NAME_ Dynamic1DArray
 
-	template <typename T>
-	class _MACRO_CLASS_NAME_ {
-
-	public:
-
-		////////////////////////
-		// DEFINITIONS BEGIN: //
-		////////////////////////
-
-		using TYPE_DATA = T ;
-
-		using TYPE_SELF
-		=	_MACRO_CLASS_NAME_
-			<	TYPE_DATA
-			>
-		; //
-
-		//////////////////////
-		// DEFINITIONS END. //
-		//////////////////////
-
-	private:
-
-		/////////////////
-		// DATA BEGIN: //
-		/////////////////
-
-		TYPE_DATA * STORE ;
-		size_t const SZ ;
-		size_t const DS ;
-		bool const allocated ;
-
-		///////////////
-		// DATA END. //
-		///////////////
-
-	public:
-
-		//////////////////////////////
-		// INTERFACE TO DATA BEGIN: //
-		//////////////////////////////
-
-		inline TYPE_DATA *
-		GET_DATA ()
-		{	return STORE ;
-		}
-
-		inline size_t
-		DIST () const
-		{	return DS ;
-		}
-
-		inline size_t
-		SIZE () const
-		{	return SZ ;
-		}
-
-		inline bool
-		ALLOCATED() const
-		{	return allocated ;
-		}
-
-		////////////////////////////
-		// INTERFACE TO DATA END. //
-		////////////////////////////
-
-	private:
-
-		///////////////////////
-		// RETRIEVING BEGIN: //
-		///////////////////////
-
-		inline TYPE_DATA &
-		GET_ELEMENT
-		(	size_t const i
-		) {	return STORE[i*DIST()] ;
-		}
-
-		inline TYPE_DATA const &
-		GET_ELEMENT
-		(	size_t const i
-		) const
-		{	return STORE[i*DIST()] ;
-		}
-
-		/////////////////////
-		// RETRIEVING END. //
-		/////////////////////
-
-	public:
-
-		////////////////////
-		// SLICING BEGIN: //
-		////////////////////
-
-		inline TYPE_SELF
-		CloneRange
-		(	size_t const begin
-		,	size_t const end
-		) const {
-
-			if (end<begin) {
-
-				return
-					CloneRange(end,begin)
-				; //
-
-			} else if (end<SIZE()) {
-
-				size_t const NEW_SIZE
-				=	end - begin + 1
-				; //
-
-				TYPE_SELF ret (NEW_SIZE) ;
-
-				for
-				(	size_t i=0
-				;	i<NEW_SIZE
-				;	i++
-				) {	ret(i)
-					=	GET_ELEMENT
-						(	i + begin
-						)
-					; //
-				}
-
-				return ret ;
-
-			} else {
-
-				return
-					CloneRange
-					(	begin
-					,	SIZE() - 1
-					)
-				;
-
-			}
-
-		}
-
-		inline TYPE_SELF
-		ViewRange
-		(	size_t const begin
-		,	size_t const end
-		) {
-
-			if (end<begin) {
-
-				return
-					ViewRange
-					(	end
-					,	begin
-					)
-				;
-
-			} else if (end<SIZE()) {
-
-				size_t const NEW_SIZE
-				=	end - begin + 1
-				;
-
-				TYPE_SELF
-					ret
-					(	&(STORE[begin])
-					,	NEW_SIZE
-					,	DIST()
-					)
-				; //
-
-				return ret ;
-
-			} else {
-
-				return
-					ViewRange
-					(	begin
-					,	SIZE() - 1
-					)
-				; //
-
-			}
-
-		}
-
-		//////////////////
-		// SLICING END. //
-		//////////////////
-
-		////////////////////////
-		// CONSTRUCTOR BEGIN: //
-		////////////////////////
-
-		_MACRO_CLASS_NAME_
-		(	size_t const _SIZE
-		) :	SZ(_SIZE)
-		,	DS(1)
-		,	allocated((SZ*DS)>0)
-		{	if (allocated) {
-				STORE
-				=	new
-						TYPE_DATA
-						[	SIZE()
-						*	DIST()
-						]
-				; //
-			}
-		}
-
-		_MACRO_CLASS_NAME_
-		(	TYPE_DATA * _STORE
-		,	size_t const _SIZE
-		,	size_t const _DIST = 1
-		) :	SZ ( _SIZE )
-		,	DS ( _DIST )
-		,	allocated (false)
-		{	STORE = _STORE ;
-		}
-
-		~_MACRO_CLASS_NAME_()
-		{	if(allocated)
-			{	delete [] STORE ;
-			}
-		}
-
-		static inline TYPE_SELF
-		CLONE
-		(	TYPE_SELF const & in
-		) {	TYPE_SELF ret(in.SIZE()) ;
-			ret = in ;
-			return ret ;
-		}
-
-		static inline TYPE_SELF
-		CLONE
-		(	std::vector <TYPE_DATA> const & in
-		) {	TYPE_SELF ret(in.size()) ;
-			ret = &(in[0]) ;
-			return ret ;
-		}
-
-		//////////////////////
-		// CONSTRUCTOR END. //
-		//////////////////////
-
-	public:
-
-		///////////////////////
-		// CONVINENCE BEGIN: //
-		///////////////////////
-
-		inline size_t
-		operator () () const
-		{	return SIZE() ;
-		}
-
-		inline TYPE_DATA &
-		operator ()
-		(	size_t const i
-		) {	return
-				GET_ELEMENT(i)
-			; //
-		}
-
-		inline TYPE_DATA const &
-		operator ()
-		(	size_t const i
-		) const {
-			return
-				GET_ELEMENT(i)
-			; //
-		}
-
-		inline void
-		operator =
-		(	TYPE_SELF const & other
-		) {	size_t I
-			=	mymin
-				(	SIZE()
-				,	other()
-				)
-			; //
-
-			for
-			(	size_t i = 0
-			;	i < I
-			;	i++
-			) {	GET_ELEMENT(i)
-				=	other(i)
-				; //
-			}
-
-		}
-
-		inline void
-		operator =
-		(	TYPE_DATA const * in
-		) {	for
-			(	size_t i = 0
-			;	i < SIZE()
-			;	i++
-			) {	GET_ELEMENT(i)
-				=	in[i]
-				; //
-			}
-		}
-
-		inline TYPE_DATA
-		operator * (TYPE_SELF const & other) const {
-			TYPE_DATA ret = 0 ;
-			size_t limit = mymin(SIZE(),other.SIZE());
-			for(size_t i=0;i<limit;i++){
-				ret += other(i) * GET_ELEMENT(i) ;
-			}
-			return
-				ret
-				/ static_cast <TYPE_DATA> (limit)
-			;
-		}
-
-		/////////////////////
-		// CONVINENCE END. //
-		/////////////////////
-
-	} ;
+template <typename TD = double, typename TI = long> class _MACRO_CLASS_NAME_ {
+
+    /////////////////////////
+    // DEFINITIONS BEGIN:{ //
+    /////////////////////////
+  public:
+    using TYPE_DATA = TD;
+    using TYPE_INT  = TI;
+    using TYPE_SELF = _MACRO_CLASS_NAME_<TYPE_DATA, TYPE_INT>;
+    ///////////////////////
+    // DEFINITIONS END.} //
+    ///////////////////////
+
+    //////////////////
+    // DATA BEGIN:{ //
+    //////////////////
+  private:
+    TYPE_DATA *    STORE;
+    TYPE_INT const SZ;
+    TYPE_INT const DS;
+    bool const     ALLOCATED;
+    ////////////////
+    // DATA END.} //
+    ////////////////
+
+    ///////////////////////////////
+    // INTERFACE TO DATA BEGIN:{ //
+    ///////////////////////////////
+  public:
+    inline TYPE_DATA *      GET_DATA() { return STORE; }
+    inline TYPE_DATA const *GET_DATA() const { return STORE; }
+    inline TYPE_INT         DIST() const { return DS; }
+    inline TYPE_INT         SIZE() const { return SZ; }
+    inline bool             GET_ALLOCATED() const { return ALLOCATED; }
+    /////////////////////////////
+    // INTERFACE TO DATA END.} //
+    /////////////////////////////
+
+    ////////////////////////
+    // RETRIEVING BEGIN:{ //
+    ////////////////////////
+  private:
+    inline TYPE_DATA &GET_ELEMENT(TYPE_INT const i) {
+        return STORE[i * DIST()];
+    }
+
+    inline TYPE_DATA const &GET_ELEMENT(TYPE_INT const i) const {
+        return STORE[i * DIST()];
+    }
+    //////////////////////
+    // RETRIEVING END.} //
+    //////////////////////
+
+    /////////////////////
+    // SLICING BEGIN:{ //
+    /////////////////////
+  public:
+    inline TYPE_SELF CloneRange(TYPE_INT const begin,
+                                TYPE_INT const end) const {
+
+        if (end < begin) {
+
+            return CloneRange(end, begin);
+
+        } else if (end < SIZE()) {
+
+            TYPE_INT const NEW_SIZE = end - begin + 1;
+
+            TYPE_SELF ret(NEW_SIZE);
+
+            for (TYPE_INT i = 0; i < NEW_SIZE; i++) {
+                ret(i) = GET_ELEMENT(i + begin);
+            }
+
+            return ret;
+
+        } else {
+
+            return CloneRange(begin, SIZE() - 1);
+        }
+    }
+
+    inline TYPE_SELF ViewRange(TYPE_INT const begin, TYPE_INT const end) {
+
+        if (end < begin) {
+
+            return ViewRange(end, begin);
+
+        } else if (end < SIZE()) {
+
+            TYPE_INT const NEW_SIZE = end - begin + 1;
+
+            TYPE_SELF
+            ret(&(STORE[begin]), NEW_SIZE, DIST());
+
+            return ret;
+
+        } else {
+
+            return ViewRange(begin, SIZE() - 1);
+        }
+    }
+
+    ///////////////////
+    // SLICING END.} //
+    ///////////////////
+
+    ////////////////////////
+    // CONVINENCE BEGIN:{ //
+    ////////////////////////
+  public:
+    inline size_t operator()() const { return SIZE(); }
+
+    inline TYPE_DATA &operator()(size_t const i) {
+        return GET_ELEMENT(i);
+    }
+
+    inline TYPE_DATA const &operator()(size_t const i) const {
+        return GET_ELEMENT(i);
+    }
+
+    inline void operator=(TYPE_SELF const &other) {
+        size_t I = mymin(SIZE(), other());
+
+        for (size_t i = 0; i < I; i++) {
+            GET_ELEMENT(i) = other(i);
+        }
+    }
+
+    inline void operator=(TYPE_DATA const *in) {
+        for (size_t i = 0; i < SIZE(); i++) {
+            GET_ELEMENT(i) = in[i];
+        }
+    }
+
+    inline TYPE_DATA operator*(TYPE_SELF const &other) const {
+        TYPE_DATA ret   = 0;
+        size_t    limit = mymin(SIZE(), other.SIZE());
+        for (size_t i = 0; i < limit; i++) { ret += other(i) * GET_ELEMENT(i); }
+        return ret / static_cast<TYPE_DATA>(limit);
+    }
+    //////////////////////
+    // CONVINENCE END.} //
+    //////////////////////
+
+    /////////////////////////
+    // CONSTRUCTOR BEGIN:{ //
+    /////////////////////////
+  public:
+    _MACRO_CLASS_NAME_(TYPE_INT const size)
+      : SZ(size), DS(1), ALLOCATED((SZ * DS) > 0) {
+        if (ALLOCATED) {
+            STORE = new TYPE_DATA[SIZE() * DIST()];
+        }
+    }
+
+    _MACRO_CLASS_NAME_(TYPE_DATA *store, TYPE_INT const size,
+                       TYPE_INT const dist = 1)
+      : SZ(size), DS(dist), ALLOCATED(false) {
+        STORE = store;
+    }
+
+    static inline TYPE_SELF CLONE(TYPE_SELF const &in) {
+        TYPE_SELF ret(in.SIZE());
+        ret = in;
+        return ret;
+    }
+
+    static inline TYPE_SELF CLONE(std::vector<TYPE_DATA> const &in) {
+        TYPE_SELF ret(in.size());
+        ret = &(in[0]);
+        return ret;
+    }
+
+    ~_MACRO_CLASS_NAME_() {
+        if (ALLOCATED) { delete[] STORE; }
+    }
+    ///////////////////////
+    // CONSTRUCTOR END.} //
+    ///////////////////////
+
+};
 
 #undef _MACRO_CLASS_NAME_
 
--- LatestHeaders/new/include/CPPFileIO.dir/symmetric_matrix.hh
+++ LatestHeaders/new/include/CPPFileIO.dir/symmetric_matrix.hh
@@ -1,98 +1,100 @@
 #ifndef _HEADER_GUARD_CPPFileIO_symmetric_matrix_
 #define _HEADER_GUARD_CPPFileIO_symmetric_matrix_
 
-////////////////////
-// Headers BEGIN: //
-////////////////////
+/////////////////////
+// Headers BEGIN:{ //
+/////////////////////
 #include "../Headers.hh"
-//////////////////
-// Headers END. //
-//////////////////
+#include "./D1.hh"
+///////////////////
+// Headers END.} //
+///////////////////
 
 #define _MACRO_CLASS_NAME_ SymmetricMatrix
 
-template <typename T>
-class _MACRO_CLASS_NAME_ {
+template <typename TD = double, typename TI = long> class _MACRO_CLASS_NAME_ {
+    /////////////////////////
+    // Definitions BEGIN:{ //
+    /////////////////////////
+  public:
+    using TYPE_DATA  = TD;
+    using TYPE_INT   = TI;
+    using TYPE_SELF  = _MACRO_CLASS_NAME_<TYPE_DATA, TYPE_INT>;
+    using TYPE_STORE = Dynamic1DArray<TYPE_DATA, TYPE_INT>;
+    ///////////////////////
+    // Definitions END.} //
+    ///////////////////////
 
-public:
+    //////////////////////////
+    // Data Element BEGIN:{ //
+    //////////////////////////
+  public:
+    TYPE_INT const N;
+    TYPE_STORE     STORE;
+    ////////////////////////
+    // Data Element END.} //
+    ////////////////////////
 
-	using TYPE_SELF = _MACRO_CLASS_NAME_ ;
-	using TYPE_DATA = T ;
+    //////////////////////////////////////
+    // Data accessing functions BEGIN:{ //
+    //////////////////////////////////////
+  private:
+    static inline TYPE_INT MAKE_MAP(TYPE_INT const y, TYPE_INT const x) {
+        return x + ((y * (y + 1)) / 2);
+    }
 
-private:
+    static inline TYPE_INT MAKE_MAP_SAFE(TYPE_INT const y, TYPE_INT const x) {
+        if (y >= x) {
+            return MAKE_MAP(y, x);
+        } else {
+            return MAKE_MAP(x, y);
+        }
+    }
 
-	TYPE_DATA * data ;
-	const size_t n ;
+  public:
+    inline TYPE_DATA &GET(TYPE_INT const y, TYPE_INT const x) {
+        return STORE(MAKE_MAP(y, x));
+    }
 
-	inline size_t
-	MakeMap (
-		size_t const y ,
-		size_t const x
-	) const {
-		return x + (y*(y+1)/2) ;
-	}
+    inline TYPE_DATA const GET(TYPE_INT const y, TYPE_INT const x) const {
+        return STORE(MAKE_MAP(y, x));
+    }
 
-	inline TYPE_DATA &
-	Get (
-		size_t const y,
-		size_t const x
-	) {
-		return data[MakeMap(y,x)] ;
-	}
+    inline TYPE_DATA &GET_SAFE(TYPE_INT const y, TYPE_INT const x) {
+        return STORE(MAKE_MAP_SAFE(y, x));
+    }
 
-	inline TYPE_DATA const
-	Get (
-		size_t const y,
-		size_t const x
-	) const {
-		return data[MakeMap(y,x)] ;
-	}
+    inline TYPE_DATA const GET_SAFE(TYPE_INT const y, TYPE_INT const x) const {
+        return STORE(MAKE_MAP_SAFE(y, x));
+    }
 
-public:
-	
-	inline TYPE_DATA &
-	operator () (
-		size_t const y,
-		size_t const x
-	) {
-		if(y>=x){
-			return Get(y,x);
-		} else {
-			return Get(x,y);
-		}
-	}
+    inline TYPE_DATA &operator()(TYPE_INT const y, TYPE_INT const x) {
+        return GET_SAFE(y, x);
+    }
 
-	inline TYPE_DATA const
-	operator () (
-		size_t const y,
-		size_t const x
-	) const {
-		if(y>=x){
-			return Get(y,x);
-		} else {
-			return Get(x,y);
-		}
-	}
+    inline TYPE_DATA const operator()(TYPE_INT const y,
+                                      TYPE_INT const x) const {
+        return GET_SAFE(x, y);
+    }
 
-	inline size_t
-	SIZE_Y () const {return n;}
+    inline TYPE_INT          SIZE_Y() const { return N; }
+    inline TYPE_INT          SIZE_X() const { return N; }
+    inline TYPE_INT          SIZE() const { return STORE(); }
+    inline TYPE_STORE &      operator()() { return STORE; }
+    inline TYPE_STORE const &operator()() const { return STORE; }
+    ////////////////////////////////////
+    // Data accessing functions END.} //
+    ////////////////////////////////////
 
-	inline size_t
-	SIZE_X () const {return n;}
-
-	_MACRO_CLASS_NAME_(size_t const _n) : n(_n) {
-		if(n>0){
-			data = new TYPE_DATA[(n+1)*(n+2)/2] ;
-		}
-	}
-
-	~_MACRO_CLASS_NAME_(){
-		if(n>0) {
-			delete [] data ;
-		}
-	}
-
-} ;
+    //////////////////////////////////////
+    // Constructor & Destructor BEGIN:{ //
+    //////////////////////////////////////
+    _MACRO_CLASS_NAME_(TYPE_INT const n) : N(n), STORE((N * (N + 1)) / 2) {}
+    ~_MACRO_CLASS_NAME_() {}
+    ////////////////////////////////////
+    // Constructor & Destructor END.} //
+    ////////////////////////////////////
+};
 
 #undef _MACRO_CLASS_NAME_
 
