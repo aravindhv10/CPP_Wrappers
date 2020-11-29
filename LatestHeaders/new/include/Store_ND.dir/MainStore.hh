41b2d17997242aeb27b57a6a9bb4ff96b34b31fb
aravind <aravindhv10@gmail.com>
2020-11-29 17:47:22 +0530

    1st version of 2D store

--- LatestHeaders/new/include/Store_ND.dir/MainStore.hh
+++ LatestHeaders/new/include/Store_ND.dir/MainStore.hh
@@ -10,6 +10,7 @@ class _MACRO_CLASS_NAME_ {
   public:
     using TYPE_FLOAT      = TF;
     using TYPE_INT        = TI;
+    using TYPE_INTS       = std::vector<TYPE_INT>;
     using TYPE_BIN_MAPPER = bin_mapper<TYPE_INT, TYPE_FLOAT>;
     using TYPE_PAIR_INT   = StaticArray::ND_ARRAY<2, TYPE_INT>;
     using TYPE_PAIR_FLOAT = StaticArray::ND_ARRAY<2, TYPE_FLOAT>;
@@ -39,24 +40,119 @@ class _MACRO_CLASS_NAME_ {
   public:
     std::string const DIRNAME;
 
-    inline std::string const NAME_DIR () {
-      mkdir(DIRNAME.c_str(),0755);
-      return DIRNAME + "/";
+    inline std::string const NAME_DIR() {
+        mkdir(DIRNAME.c_str(), 0755);
+        return DIRNAME + "/";
     }
 
-    inline std::string const NAME_INDEX () {
-      return NAME_DIR() + "index" ;
-    }
+    inline std::string const NAME_INDEX() { return NAME_DIR() + "index"; }
 
-    inline std::string const NAME_STORE () {
-      return NAME_DIR() + "store" ;
-    }
-
+    inline std::string const NAME_STORE() { return NAME_DIR() + "store"; }
 
   public:
-    _MACRO_CLASS_NAME_(std::string const dirname): DIRNAME(dirname){}
+    CPPFileIO::FileArray<char> BUFFER_INDEX;
+    TYPE_INDEX *               INDEX;
 
-    ~_MACRO_CLASS_NAME_(){}
+    inline void ALLOCATE_READ_INDEX() {
+        BUFFER_INDEX.writeable(false);
+        INDEX =
+          reinterpret_cast<TYPE_INDEX>(&(BUFFER_INDEX(0, sizeof(TYPE_INDEX))));
+    }
+
+    inline void ALLOCATE_WRITE_INDEX() {
+        BUFFER_INDEX.writeable(true);
+        INDEX =
+          reinterpret_cast<TYPE_INDEX>(&(BUFFER_INDEX(0, sizeof(TYPE_INDEX))));
+    }
+
+    inline void TRUNCATE_INDEX() {
+        BUFFER_INDEX.size(sizeof(TYPE_INDEX));
+        ALLOCATE_READ_INDEX();
+    }
+
+    inline void SET_RANGE(TYPE_FLOAT const x1, TYPE_FLOAT const x2,
+                          TYPE_FLOAT const y1, TYPE_FLOAT const y2) {
+        INDEX->SET_RANGE(x1, x2, y1, y2);
+    }
+
+    inline TYPE_PAIR_INT GET_BINS(TYPE_FLOAT const y,
+                                  TYPE_FLOAT const x) const {
+        return INDEX->GET_BINS(y, x);
+    }
+
+    inline TYPE_INT FLATTEN_MAP(TYPE_FLOAT const y, TYPE_FLOAT const x) const {
+        return INDEX->FLATTEN_MAP(y, x);
+    }
+
+    inline void ZERO_COUNTS() { INDEX->ZERO_COUNTS(); }
+
+    inline void EVAL_CUMULATIVE() { INDEX->EVAL_CUMULATIVE(); }
+
+  public:
+    CPPFileIO::FileArray<TYPE_STORE_ELEMENT> STORE;
+
+    template <typename Reader, typename Converter>
+    inline void WRITE_STORE(Reader reader, Converter converter) {
+        ALLOCATE_WRITE_INDEX();
+        size_t const limit = reader();
+
+        /* Setting the range: */ {
+            TYPE_PAIR_FLOAT min, max;
+            /* check the 1st element: */ {
+                auto const &       element = reader(0);
+                TYPE_STORE_ELEMENT tmp;
+                tmp.vals = converter(element);
+                min      = tmp.vals;
+                max      = tmp.vals;
+            }
+            for (size_t i = 1; i < limit; i++) {
+                auto const &       element = reader(i);
+                TYPE_STORE_ELEMENT tmp;
+                tmp.vals = converter(element);
+                min[0]   = CPPFileIO::mymin(min[0], tmp.vals[0]);
+                min[1]   = CPPFileIO::mymin(min[1], tmp.vals[1]);
+                max[0]   = CPPFileIO::mymax(max[0], tmp.vals[0]);
+                max[1]   = CPPFileIO::mymax(max[1], tmp.vals[1]);
+            }
+            INDEX->SET_RANGE(min[0], min[1], max[0], max[1]);
+        }
+
+        /* Pass 1: */ {
+            ZERO_COUNTS();
+            for (size_t i = 0; i < limit; i++) {
+                auto const &       element = reader(i);
+                TYPE_STORE_ELEMENT tmp;
+                tmp.vals  = converter(element);
+                tmp.index = i;
+                INDEX->ADD_LOCATION(tmp.vals[0], tmp.vals[1]);
+            }
+            EVAL_CUMULATIVE();
+            ZERO_COUNTS();
+        }
+
+        /* Pass 2: */ {
+            STORE.writeable(true);
+            for (size_t i = 0; i < limit; i++) {
+                auto const &       element = reader(i);
+                TYPE_STORE_ELEMENT tmp;
+                tmp.vals  = converter(element);
+                tmp.index = i;
+                TYPE_INT const idx_loc =
+                  INDEX->ADD_LOCATION(tmp.vals[0], tmp.vals[1]);
+                STORE(idx_loc) = tmp;
+            }
+            STORE.writeable(false);
+        }
+
+        STORE.size(limit);
+        TRUNCATE_INDEX();
+    }
+
+  public:
+    _MACRO_CLASS_NAME_(std::string const dirname)
+      : DIRNAME(dirname), BUFFER_INDEX(NAME_INDEX), STORE(NAME_STORE()) {}
+
+    ~_MACRO_CLASS_NAME_() {}
 };
 #undef _MACRO_CLASS_NAME_
 
