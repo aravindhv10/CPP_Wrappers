3276b699463694c40e5c1bb0b65e254e88e63a15
aravind <aravindhv10@gmail.com>
2020-12-05 22:47:16 +0530

    Started Writing heap class

--- LatestHeaders/new/include/Spherical_Geometry_Clustering.dir/D2_Store.hh
+++ LatestHeaders/new/include/Spherical_Geometry_Clustering.dir/D2_Store.hh
@@ -1,12 +1,59 @@
 #ifndef _HEADER_GUARD_Spherical_Geometry_Clustering_D2_Store_
 #define _HEADER_GUARD_Spherical_Geometry_Clustering_D2_Store_
 
+#include "../Read_Show_Functions.hh"
 #include "./PolarCoordinates.hh"
+
+/////////////////////////////
+// Storage Element BEGIN:{ //
+/////////////////////////////
+#define _MACRO_CLASS_NAME_ D2GPS_Element
+
+template <typename TF, typename TI> class _MACRO_CLASS_NAME_ {
+  public:
+    using TYPE_FLOAT = TF;
+    using TYPE_INT   = TI;
+    using TYPE_SELF  = _MACRO_CLASS_NAME_<TYPE_FLOAT, TYPE_INT>;
+    using TYPE_POINT = D2GPS_Coordinates<TYPE_FLOAT, TYPE_INT>;
+    using TYPE_ULONG = unsigned long;
+
+  public:
+    TYPE_POINT POINT;
+    TYPE_ULONG Z_CURVE;
+    TYPE_INT   INDEX;
+
+  public:
+    inline char Compare(TYPE_SELF const &other) const {
+        return Read_Show_Functions::Compare(Z_CURVE, other.Z_CURVE);
+    }
+
+    inline bool operator>(TYPE_SELF const &other) const {
+        return (Compare(other) > 0);
+    }
+
+    inline bool operator<(TYPE_SELF const &other) const {
+        return (Compare(other) < 0);
+    }
+
+    inline bool operator==(TYPE_SELF const &other) const {
+        return (Compare(other) == 0);
+    }
+
+  public:
+    _MACRO_CLASS_NAME_() {}
+    ~_MACRO_CLASS_NAME_() {}
+};
+
+#undef _MACRO_CLASS_NAME_
+///////////////////////////
+// Storage Element END.} //
+///////////////////////////
 
 //////////////////////////
 // Storage node BEGIN:{ //
 //////////////////////////
 #define _MACRO_CLASS_NAME_ D2GPS_Node
+
 template <typename TF, typename TI> class _MACRO_CLASS_NAME_ {
   public:
     using TYPE_FLOAT      = TF;
@@ -25,12 +72,23 @@ template <typename TF, typename TI> class _MACRO_CLASS_NAME_ {
 
   public:
     inline TYPE_INT LENGTH() const { return (RANGE[1] - RANGE[0] + 1); }
-    inline bool IS_LEAF() const { return (LENGTH() < LENGTH_LIMIT()); }
+    inline bool     IS_LEAF() const { return (LENGTH() < LENGTH_LIMIT()); }
 
   public:
+    _MACRO_CLASS_NAME_(TYPE_PAIR_INT const in) {
+        RANGE[0] = in[0];
+        RANGE[1] = in[1];
+    }
+
+    _MACRO_CLASS_NAME_(TYPE_INT const start, TYPE_INT const end) {
+        RANGE[0] = start;
+        RANGE[1] = end;
+    }
+
     _MACRO_CLASS_NAME_() {}
     ~_MACRO_CLASS_NAME_() {}
 };
+
 #undef _MACRO_CLASS_NAME_
 ////////////////////////
 // Storage node END.} //
@@ -43,9 +101,72 @@ template <typename TF, typename TI> class _MACRO_CLASS_NAME_ {
 
 template <typename TF, typename TI> class _MACRO_CLASS_NAME_ {
   public:
-    using TYPE_FLOAT = TF;
-    using TYPE_INT   = TI;
-    using TYPE_SELF  = _MACRO_CLASS_NAME_<TYPE_FLOAT, TYPE_INT>;
+    using TYPE_FLOAT      = TF;
+    using TYPE_INT        = TI;
+    using TYPE_SELF       = _MACRO_CLASS_NAME_<TYPE_FLOAT, TYPE_INT>;
+    using TYPE_POINT      = D2GPS_Coordinates<TYPE_FLOAT, TYPE_INT>;
+    using TYPE_BOX        = D2GPS_Box<TYPE_FLOAT, TYPE_INT>;
+    using TYPE_PAIR_INT   = StaticArray::ND_ARRAY<2, TYPE_INT>;
+    using TYPE_PAIR_FLOAT = StaticArray::ND_ARRAY<2, TYPE_FLOAT>;
+    using TYPE_NODE       = D2GPS_Node<TYPE_FLOAT, TYPE_INT>;
+    using TYPE_ELEMENT    = D2GPS_Element<TYPE_FLOAT, TYPE_INT>;
+
+    static inline TYPE_INT INDEX_PARENT(TYPE_INT const i) {
+        return (i - 1) / 2;
+    }
+
+    static inline TYPE_INT INDEX_LEFT_CHILD(TYPE_INT const i) {
+        return (2 * i) + 1;
+    }
+
+    static inline TYPE_INT INDEX_RIGHT_CHILD(TYPE_INT const i) {
+        return (2 * i) + 2;
+    }
+
+  public:
+    CPPFileIO::FileArray<TYPE_ELEMENT> STORE;
+    CPPFileIO::FileArray<TYPE_NODE>    HEAP;
+    TYPE_INT MAX_HEAP_SIZE;
+
+  public:
+    inline void MAKE_HEAP(TYPE_NODE &parent_node, TYPE_INT const index_node) {
+        if (parent_node.IS_LEAF()) {
+            auto const start = parent_node.RANGE[0];
+            auto const end = parent_node.RANGE[1];
+            auto const length = parent_node.LENGTH();
+            TYPE_ELEMENT * buffer = &(STORE(start, length)) ;
+            parent_node.BBOX = buffer[0];
+            for(TYPE_INT i=1;i<length;i++) {
+                parent_node.BBOX += buffer[i];
+            }
+        } else {
+            TYPE_INT const index_left  = INDEX_LEFT_CHILD(index_node);
+            TYPE_INT const index_right = INDEX_RIGHT_CHILD(index_node);
+            TYPE_INT const mid =
+              (parent_node.RANGE[0] + parent_node.RANGE[1]) / 2;
+            TYPE_NODE left, right;
+            left.RANGE[0]  = parent_node.RANGE[0];
+            left.RANGE[1]  = mid;
+            right.RANGE[0] = mid + 1;
+            right.RANGE[1] = parent_node.RANGE[1];
+            MAKE_HEAP(left, index_left);
+            MAKE_HEAP(right, index_right);
+            parent_node.BBOX = left.BBOX + right.BBOX;
+        }
+        HEAP(index_node) = parent_node;
+        MAX_HEAP_SIZE = CPPFileIO::mymax(MAX_HEAP_SIZE, index_node);
+    }
+
+    inline void MAKE_HEAP(){
+        MAX_HEAP_SIZE = 0;
+        HEAP.writeable(true);
+        TYPE_NODE node;
+        node.RANGE[0] = 0;
+        node.RANGE[1] = STORE() - 1;
+        MAKE_HEAP(node, 0);
+        HEAP.writeable(false);
+        HEAP.size(MAX_HEAP_SIZE);
+    }
 
   public:
     _MACRO_CLASS_NAME_() {}
