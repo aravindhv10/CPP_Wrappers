0bec8a9206c63ee2c677f18779d04a758ff31cd1
Aravind <aravindhv10@gmail.com>
2020-12-09 08:57:40 +0530

    Minor cleanup

--- LatestHeaders/new/include/Spherical_Geometry_Clustering.dir/D2_Store.hh
+++ LatestHeaders/new/include/Spherical_Geometry_Clustering.dir/D2_Store.hh
@@ -1,8 +1,15 @@
 #ifndef _HEADER_GUARD_Spherical_Geometry_Clustering_D2_Store_
 #define _HEADER_GUARD_Spherical_Geometry_Clustering_D2_Store_
 
+/////////////////////
+// Headers BEGIN:{ //
+/////////////////////
 #include "../Read_Show_Functions.hh"
 #include "./PolarCoordinates.hh"
+#include "./Box.hh"
+///////////////////
+// Headers END.} //
+///////////////////
 
 /////////////////////////////
 // Storage Element BEGIN:{ //
@@ -111,6 +118,7 @@ class _MACRO_CLASS_NAME_ {
     using TYPE_PAIR_INT   = StaticArray::ND_ARRAY<2, TYPE_INT>;
     using TYPE_PAIR_FLOAT = StaticArray::ND_ARRAY<2, TYPE_FLOAT>;
     using TYPE_ELEMENT    = D2GPS_Element<TYPE_FLOAT, TYPE_INT>;
+    using TYPE_ELEMENTS   = std::vector<TYPE_ELEMENT>;
 
     static TYPE_INT constexpr LENGTH_LIMIT() { return LEAF_LENGTH; }
     using TYPE_SELF = _MACRO_CLASS_NAME_<TYPE_FLOAT, TYPE_INT, LENGTH_LIMIT()>;
@@ -169,47 +177,6 @@ class _MACRO_CLASS_NAME_ {
     }
 
   private:
-    inline void MAKE_HEAP_OLD(TYPE_NODE &    parent_node,
-                              TYPE_INT const index_node) {
-        if (parent_node.IS_LEAF()) {
-            auto const    start  = parent_node.RANGE[0];
-            auto const    end    = parent_node.RANGE[1];
-            auto const    length = parent_node.LENGTH();
-            TYPE_ELEMENT *buffer = &(STORE(start, length));
-            parent_node.BBOX     = buffer[0].POINT;
-            for (TYPE_INT i = 1; i < length; i++) {
-                parent_node.BBOX += buffer[i].POINT;
-            }
-        } else {
-            TYPE_INT const index_left  = INDEX_LEFT_CHILD(index_node);
-            TYPE_INT const index_right = INDEX_RIGHT_CHILD(index_node);
-            TYPE_INT const mid =
-              (parent_node.RANGE[0] + parent_node.RANGE[1]) / 2;
-            TYPE_NODE left, right;
-            left.RANGE[0]  = parent_node.RANGE[0];
-            left.RANGE[1]  = mid;
-            right.RANGE[0] = mid + 1;
-            right.RANGE[1] = parent_node.RANGE[1];
-            MAKE_HEAP_OLD(left, index_left);
-            MAKE_HEAP_OLD(right, index_right);
-            parent_node.BBOX = left.BBOX + right.BBOX;
-        }
-        HEAP(index_node) = parent_node;
-        MAX_HEAP_SIZE    = CPPFileIO::mymax(MAX_HEAP_SIZE, index_node);
-    }
-
-    inline void MAKE_HEAP_OLD() {
-        HEAP.size(0);
-        MAX_HEAP_SIZE = 0;
-        HEAP.writeable(true);
-        TYPE_NODE node;
-        node.RANGE[0] = 0;
-        node.RANGE[1] = STORE() - 1;
-        MAKE_HEAP_OLD(node, 0);
-        HEAP.size(MAX_HEAP_SIZE);
-        HEAP.writeable(false);
-    }
-
     inline void MAKE_HEAP() {
         HEAP.size(0);
         MAX_HEAP_SIZE = 0;
@@ -264,7 +231,7 @@ class _MACRO_CLASS_NAME_ {
             if (!element.IS_LEAF()) {
                 TYPE_NODE const *children = &(HEAP(INDEX_LEFT_CHILD(i), 2));
                 element.BBOX              = children[0].BBOX + children[1].BBOX;
-                HEAP(i) = element;
+                HEAP(i)                   = element;
             }
         }
 
@@ -272,120 +239,57 @@ class _MACRO_CLASS_NAME_ {
         HEAP.writeable(false);
     }
 
-    inline void RETRIEVE_ELEMENTS(TYPE_POINTS &places, TYPE_BOX const &in) {
-        std::stack<TYPE_INT> stack;
-        stack.push(0);
-        while (!stack.empty()) {
-            TYPE_INT const index = stack.top();
-            stack.pop();
-            TYPE_NODE const element    = HEAP(index);
-            bool const      intersects = element.BBOX(in);
-            if (intersects) {
-                if (element.IS_LEAF()) {
-                    TYPE_INT const      start  = element.RANGE[0];
-                    TYPE_INT const      end    = element.RANGE[1];
-                    TYPE_INT const      length = end - start + 1;
-                    TYPE_ELEMENT const *buffer = &(STORE(start, length));
-                    for (TYPE_INT i = 0; i < length; i++) {
-                        bool const toinclude = in(buffer[i].POINT);
-                        if (toinclude) { places.push_back(buffer[i].POINT); }
-                    }
-                } else {
-                    TYPE_INT const leftchild = INDEX_LEFT_CHILD(index);
-                    stack.push(leftchild);
-                    stack.push(leftchild + 1);
-                }
-            }
-        }
+#define _MACRO_DEFINE_RETRIEVE_(Type, PushStatement)                           \
+  private:                                                                     \
+    inline void RETRIEVE_ELEMENTS(Type &places, TYPE_BOX const &in) {          \
+        std::stack<TYPE_INT> stack;                                            \
+        stack.push(0);                                                         \
+        while (!stack.empty()) {                                               \
+            TYPE_INT const index = stack.top();                                \
+            stack.pop();                                                       \
+            TYPE_NODE const element    = HEAP(index);                          \
+            bool const      intersects = element.BBOX(in);                     \
+            if (intersects) {                                                  \
+                if (element.IS_LEAF()) {                                       \
+                    TYPE_INT const      start  = element.RANGE[0];             \
+                    TYPE_INT const      end    = element.RANGE[1];             \
+                    TYPE_INT const      length = end - start + 1;              \
+                    TYPE_ELEMENT const *buffer = &(STORE(start, length));      \
+                    for (TYPE_INT i = 0; i < length; i++) {                    \
+                        bool const toinclude = in(buffer[i].POINT);            \
+                        if (toinclude) { PushStatement; }                      \
+                    }                                                          \
+                } else {                                                       \
+                    TYPE_INT const leftchild = INDEX_LEFT_CHILD(index);        \
+                    stack.push(leftchild);                                     \
+                    stack.push(leftchild + 1);                                 \
+                }                                                              \
+            }                                                                  \
+        }                                                                      \
+    }                                                                          \
+                                                                               \
+  public:                                                                      \
+    inline void operator()(Type &indices, TYPE_BOX const &in) {                \
+        indices.clear();                                                       \
+        RETRIEVE_ELEMENTS(indices, in);                                        \
+    }                                                                          \
+    inline void operator()(Type &indices, TYPE_FLOAT const lat1,               \
+                           TYPE_FLOAT const lon1, TYPE_FLOAT const lat2,       \
+                           TYPE_FLOAT const lon2) {                            \
+        indices.clear();                                                       \
+        TYPE_BOX inbox;                                                        \
+        inbox.MIN.latitude  = CPPFileIO::mymin(lat1, lat2);                    \
+        inbox.MIN.longitude = CPPFileIO::mymin(lon1, lon2);                    \
+        inbox.MAX.latitude  = CPPFileIO::mymax(lat1, lat2);                    \
+        inbox.MAX.longitude = CPPFileIO::mymax(lon1, lon2);                    \
+        RETRIEVE_ELEMENTS(indices, inbox);                                     \
     }
 
-    inline void RETRIEVE_ELEMENTS(TYPE_INTS &indices, TYPE_BOX const &in) {
-        std::stack<TYPE_INT> stack;
-        stack.push(0);
-        while (!stack.empty()) {
-            TYPE_INT const index = stack.top();
-            stack.pop();
-            TYPE_NODE const element    = HEAP(index);
-            bool const      intersects = element.BBOX(in);
-            if (intersects) {
-                if (element.IS_LEAF()) {
-                    TYPE_INT const      start  = element.RANGE[0];
-                    TYPE_INT const      end    = element.RANGE[1];
-                    TYPE_INT const      length = end - start + 1;
-                    TYPE_ELEMENT const *buffer = &(STORE(start, length));
-                    for (TYPE_INT i = 0; i < length; i++) {
-                        bool const toinclude = in(buffer[i].POINT);
-                        if (toinclude) { indices.push_back(buffer[i].INDEX); }
-                    }
-                } else {
-                    TYPE_INT const leftchild = INDEX_LEFT_CHILD(index);
-                    stack.push(leftchild);
-                    stack.push(leftchild + 1);
-                }
-            }
-        }
-        /* DEBUGGING */ if (false) {
-            TYPE_INT const limit_store = STORE();
-            TYPE_INTS      checking;
-            for (TYPE_INT i = 0; i < limit_store; i++) {
-                bool const intersects = in(STORE(i).POINT);
-                if (intersects) { checking.push_back(STORE(i).INDEX); }
-            }
-            if (indices.size() > 0) {
-                std::sort(indices.begin(), indices.end());
-            }
-            if (checking.size() > 0) {
-                std::sort(checking.begin(), checking.end());
-            }
-            if (checking.size() == indices.size()) {
-                for (size_t i = 0; i < indices.size(); i++) {
-                    printf("CHECK INTERNAL...: %ld %ld\n", indices[i],
-                           checking[i]);
-                }
-            } else {
-                printf("RESULTS ARE WRONG!!!\n");
-            }
-        }
-    }
+    _MACRO_DEFINE_RETRIEVE_(TYPE_POINTS, places.push_back(buffer[i].POINT))
+    _MACRO_DEFINE_RETRIEVE_(TYPE_INTS, places.push_back(buffer[i].INDEX))
+    _MACRO_DEFINE_RETRIEVE_(TYPE_ELEMENTS, places.push_back(buffer[i]))
 
-    inline void RETRIEVE_ELEMENTS_OLD(TYPE_INTS &indices, TYPE_BOX const &in,
-                                      TYPE_INT const i) {
-
-        auto const &heap = HEAP(i);
-        bool const  ret  = heap.BBOX(in);
-
-        printf("1\n");
-        if (ret) {
-            if (HEAP(i).IS_LEAF()) {
-                printf("2\n");
-                TYPE_INT const start  = heap.RANGE[0];
-                TYPE_INT const end    = heap.RANGE[1];
-                TYPE_INT const length = end - start + 1;
-                printf("DEBUG: %ld %ld %ld %ld\n", start, end, length, STORE());
-                TYPE_ELEMENT const *buffer = &(STORE(start, length));
-                printf("2.1\n");
-                for (TYPE_INT j = 0; j < length; j++) {
-                    printf("DEBUG j = %ld\n", j);
-                    bool const res = in(buffer[j].POINT);
-                    printf("2.2\n");
-                    if (res) {
-                        printf("2.3\n");
-                        printf("adding %ld (%lf, %lf)\n", buffer[j].INDEX,
-                               buffer[j].POINT.latitude,
-                               buffer[i].POINT.longitude);
-                        printf("2.4\n");
-                        indices.push_back(buffer[j].INDEX);
-                    }
-                    printf("2.4\n");
-                }
-            } else {
-                printf("3\n");
-                RETRIEVE_ELEMENTS_OLD(indices, in, INDEX_LEFT_CHILD(i));
-                RETRIEVE_ELEMENTS_OLD(indices, in, INDEX_RIGHT_CHILD(i));
-                printf("4\n");
-            }
-        }
-    }
+#undef _MACRO_DEFINE_RETRIEVE_
 
   public:
     template <typename Reader> inline void MAKE_STORE(Reader &reader) {
@@ -409,54 +313,13 @@ class _MACRO_CLASS_NAME_ {
         MAKE_HEAP();
     }
 
-    inline void operator()(TYPE_INTS &indices, TYPE_BOX const &in) {
-        indices.clear();
-        RETRIEVE_ELEMENTS(indices, in);
-    }
+    inline void read_all_heap() { HEAP(0, HEAP()); }
 
-    inline void operator()(TYPE_POINTS &points, TYPE_BOX const &in) {
-        points.clear();
-        RETRIEVE_ELEMENTS(points, in);
-    }
+    inline void read_all_store() { STORE(0, STORE()); }
 
-    inline void operator()(TYPE_INTS &indices, TYPE_FLOAT const lat1,
-                           TYPE_FLOAT const lon1, TYPE_FLOAT const lat2,
-                           TYPE_FLOAT const lon2) {
-
-        indices.clear();
-        TYPE_BOX inbox;
-        inbox.MIN.latitude  = CPPFileIO::mymin(lat1, lat2);
-        inbox.MIN.longitude = CPPFileIO::mymin(lon1, lon2);
-        inbox.MAX.latitude  = CPPFileIO::mymax(lat1, lat2);
-        inbox.MAX.longitude = CPPFileIO::mymax(lon1, lon2);
-        RETRIEVE_ELEMENTS(indices, inbox);
-    }
-
-
-    inline void operator()(TYPE_POINTS &points, TYPE_FLOAT const lat1,
-                           TYPE_FLOAT const lon1, TYPE_FLOAT const lat2,
-                           TYPE_FLOAT const lon2) {
-
-        points.clear();
-        TYPE_BOX inbox;
-        inbox.MIN.latitude  = CPPFileIO::mymin(lat1, lat2);
-        inbox.MIN.longitude = CPPFileIO::mymin(lon1, lon2);
-        inbox.MAX.latitude  = CPPFileIO::mymax(lat1, lat2);
-        inbox.MAX.longitude = CPPFileIO::mymax(lon1, lon2);
-        RETRIEVE_ELEMENTS(points, inbox);
-    }
-
-    inline void read_all_heap() {
-        HEAP(0,HEAP());
-    }
-
-    inline void read_all_store() {
-        STORE(0,STORE());
-    }
-
-    inline void read_all () {
-      read_all_heap();
-      read_all_store();
+    inline void read_all() {
+        read_all_heap();
+        read_all_store();
     }
 
   public:
--- LatestHeaders/new/include/Spherical_Geometry_Clustering.dir/PolarCoordinates.hh
+++ LatestHeaders/new/include/Spherical_Geometry_Clustering.dir/PolarCoordinates.hh
@@ -232,6 +232,14 @@ template <typename TF = double, typename TI = long> class _MACRO_CLASS_NAME_ {
         return this->HaverSineDistance(in);
     }
 
+    inline TYPE_ULONG operator () () const {
+      return z_curve();
+    }
+    //////////////////////
+    // Convinence END.} //
+    //////////////////////
+
+  public:
     _MACRO_CLASS_NAME_(TYPE_FLOAT const inlat, TYPE_FLOAT const inlon)
       : latitude(inlat), longitude(inlon) {}
 
@@ -240,9 +248,6 @@ template <typename TF = double, typename TI = long> class _MACRO_CLASS_NAME_ {
 
     _MACRO_CLASS_NAME_() {}
     ~_MACRO_CLASS_NAME_() {}
-    //////////////////////
-    // Convinence END.} //
-    //////////////////////
 };
 
 #undef _MACRO_CLASS_NAME_
@@ -250,102 +255,5 @@ template <typename TF = double, typename TI = long> class _MACRO_CLASS_NAME_ {
 ///////////////////////////
 // GPS Coordinates END.} //
 ///////////////////////////
-
-///////////////////////
-// GPS Boxes BEGIN:{ //
-///////////////////////
-
-#define _MACRO_CLASS_NAME_ D2GPS_Box
-
-template <typename TF = double, typename TI = long> class _MACRO_CLASS_NAME_ {
-  public:
-    using TYPE_FLOAT = TF;
-    using TYPE_INT   = TI;
-    using TYPE_SELF  = _MACRO_CLASS_NAME_<TYPE_FLOAT, TYPE_INT>;
-    using TYPE_POINT = D2GPS_Coordinates<TYPE_FLOAT, TYPE_INT>;
-
-  public:
-    TYPE_POINT MIN, MAX;
-
-  public:
-    inline TYPE_POINT P1() const { return MIN; }
-    inline TYPE_POINT P2() const {
-        TYPE_POINT ret;
-        ret.latitude  = MAX.latitude;
-        ret.longitude = MIN.longitude;
-        return ret;
-    }
-    inline TYPE_POINT P3() const {
-        TYPE_POINT ret;
-        ret.latitude  = MIN.latitude;
-        ret.longitude = MAX.longitude;
-        return ret;
-    }
-    inline TYPE_POINT P4() const { return MAX; }
-
-  public:
-    inline void operator=(TYPE_POINT const in) {
-        MIN = in;
-        MAX = in;
-    }
-    inline void operator+=(TYPE_POINT const &in) {
-        MIN.latitude  = CPPFileIO::mymin(MIN.latitude, in.latitude);
-        MIN.longitude = CPPFileIO::mymin(MIN.longitude, in.longitude);
-        MAX.latitude  = CPPFileIO::mymax(MAX.latitude, in.latitude);
-        MAX.longitude = CPPFileIO::mymax(MAX.longitude, in.longitude);
-    }
-    inline TYPE_SELF operator+(TYPE_POINT const &in) const {
-        TYPE_SELF ret = this[0];
-        ret += in;
-        return ret;
-    }
-    inline bool operator()(TYPE_POINT const &in) const {
-        bool const ret =
-          (MIN.latitude <= in.latitude) && (in.latitude <= MAX.latitude) &&
-          (MIN.longitude <= in.longitude) && (in.longitude <= MAX.longitude);
-        return ret;
-    }
-
-  public:
-    inline void operator=(TYPE_SELF const &other) {
-        MIN = other.MIN;
-        MAX = other.MAX;
-    }
-    inline void operator+=(TYPE_SELF const &other) {
-        MIN.latitude  = CPPFileIO::mymin(MIN.latitude, other.MIN.latitude);
-        MIN.longitude = CPPFileIO::mymin(MIN.longitude, other.MIN.longitude);
-        MAX.latitude  = CPPFileIO::mymax(MAX.latitude, other.MAX.latitude);
-        MAX.longitude = CPPFileIO::mymax(MAX.longitude, other.MAX.longitude);
-    }
-    inline TYPE_SELF operator+(TYPE_SELF const &other) const {
-        TYPE_SELF ret;
-        ret.MIN.latitude = CPPFileIO::mymin(MIN.latitude, other.MIN.latitude);
-        ret.MIN.longitude =
-          CPPFileIO::mymin(MIN.longitude, other.MIN.longitude);
-        ret.MAX.latitude = CPPFileIO::mymax(MAX.latitude, other.MAX.latitude);
-        ret.MAX.longitude =
-          CPPFileIO::mymax(MAX.longitude, other.MAX.longitude);
-        return ret;
-    }
-
-  public:
-    inline bool operator()(TYPE_SELF const &in) const {
-        bool const ret = in(this->P1()) || in(this->P2()) || in(this->P3()) ||
-                         in(this->P4()) || this[0](in.P1()) ||
-                         this[0](in.P2()) || this[0](in.P3()) ||
-                         this[0](in.P4());
-        return ret;
-    }
-
-  public:
-    _MACRO_CLASS_NAME_() {}
-    ~_MACRO_CLASS_NAME_() {}
-};
-
-#undef _MACRO_CLASS_NAME_
-
-/////////////////////
-// GPS Boxes END.} //
-/////////////////////
 
 #endif
