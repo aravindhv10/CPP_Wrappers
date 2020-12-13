a28c3b42179bd0db250f86a0a6edd2c361ecc1ce
Aravind <aravindhv10@gmail.com>
2020-12-13 16:28:54 +0530

    Started generalization

--- LatestHeaders/new/include/Read_Show_Functions.dir/Read.hh
+++ LatestHeaders/new/include/Read_Show_Functions.dir/Read.hh
@@ -10,17 +10,41 @@
 // Header END. //
 /////////////////
 
+////////////////////
+// String BEGIN:{ //
+////////////////////
 template <size_t n>
 inline void Read(StaticArray::ND_ARRAY<n, char> &dest, std::string const &src) {
     std::memset(/* void *s = */ reinterpret_cast<void *>(dest.GET_DATA()),
-                /* int c = */ 0, /* size_t n = */ dest.SIZE()); //
+                /* int c = */ 0, /* size_t n = */ dest.SIZE());
+
     size_t const limit = CPPFileIO::mymin(
       static_cast<size_t>(src.size()), static_cast<size_t>(dest.SIZE() - 1)); //
+
     memcpy(/* void *dest = */ reinterpret_cast<void *>(dest.GET_DATA()),
            /* const void *src = */ reinterpret_cast<void const *>(src.c_str()),
-           /* size_t n = */ limit); //
+           /* size_t n = */ limit);
 }
 
+template <size_t n>
+inline void Read(StaticArray::ND_ARRAY<n, char> &dest, char const *src) {
+    std::memset(/* void *s = */ reinterpret_cast<void *>(dest.GET_DATA()),
+                /* int c = */ 0, /* size_t n = */ dest.SIZE());
+
+    size_t const limit = CPPFileIO::mymin(static_cast<size_t>(strlen(src)),
+                                          static_cast<size_t>(dest.SIZE() - 1));
+
+    memcpy(/* void *dest = */ reinterpret_cast<void *>(dest.GET_DATA()),
+           /* const void *src = */ reinterpret_cast<void const *>(src),
+           /* size_t n = */ limit);
+}
+//////////////////
+// String END.} //
+//////////////////
+
+///////////////////
+// Float BEGIN:{ //
+///////////////////
 static inline void Read(float &dest, std::string const &src) {
     if (src.size() < 1) {
         dest = NAN;
@@ -29,6 +53,20 @@ static inline void Read(float &dest, std::string const &src) {
     }
 }
 
+static inline void Read(float &dest, char const *src) {
+    if (strlen(src) < 1) {
+        dest = NAN;
+    } else {
+        dest = std::stof(src);
+    }
+}
+/////////////////
+// Float END.} //
+/////////////////
+
+////////////////////
+// double BEGIN:{ //
+////////////////////
 static inline void Read(double &dest, std::string const &src) {
     if (src.size() < 1) {
         dest = NAN;
@@ -37,6 +75,20 @@ static inline void Read(double &dest, std::string const &src) {
     }
 }
 
+static inline void Read(double &dest, char const *src) {
+    if (strlen(src) < 1) {
+        dest = NAN;
+    } else {
+        dest = std::stod(src);
+    }
+}
+//////////////////
+// double END.} //
+//////////////////
+
+//////////////////
+// char BEGIN:{ //
+//////////////////
 static inline void Read(char &dest, std::string const &src) {
     if (src.size() < 1) {
         dest = 0;
@@ -45,6 +97,20 @@ static inline void Read(char &dest, std::string const &src) {
     }
 }
 
+static inline void Read(char &dest, char const *src) {
+    if (strlen(src) < 1) {
+        dest = 0;
+    } else {
+        dest = src[0];
+    }
+}
+////////////////
+// char END.} //
+////////////////
+
+///////////////////////////
+// unsigned char BEGIN:{ //
+///////////////////////////
 static inline void Read(unsigned char &dest, std::string const &src) {
     if (src.size() < 1) {
         dest = 0;
@@ -53,6 +119,20 @@ static inline void Read(unsigned char &dest, std::string const &src) {
     }
 }
 
+static inline void Read(unsigned char &dest, char const *src) {
+    if (strlen(src) < 1) {
+        dest = 0;
+    } else {
+        dest = static_cast<unsigned char>(std::stoi(src));
+    }
+}
+/////////////////////////
+// unsigned char END.} //
+/////////////////////////
+
+//////////////////////////
+// unsigned int BEGIN:{ //
+//////////////////////////
 static inline void Read(unsigned int &dest, std::string const &src) {
     if (src.size() < 1) {
         dest = -999999;
@@ -61,6 +141,20 @@ static inline void Read(unsigned int &dest, std::string const &src) {
     }
 }
 
+static inline void Read(unsigned int &dest, char const *src) {
+    if (strlen(src) < 1) {
+        dest = -999999;
+    } else {
+        sscanf(src, "%u", &dest);
+    }
+}
+////////////////////////
+// unsigned int END.} //
+////////////////////////
+
+/////////////////
+// int BEGIN:{ //
+/////////////////
 static inline void Read(int &dest, std::string const &src) {
     if (src.size() < 1) {
         dest = -999999;
@@ -69,6 +163,20 @@ static inline void Read(int &dest, std::string const &src) {
     }
 }
 
+static inline void Read(int &dest, char const *src) {
+    if (strlen(src) < 1) {
+        dest = -999999;
+    } else {
+        dest = std::stoi(src);
+    }
+}
+///////////////
+// int END.} //
+///////////////
+
+//////////////////
+// long BEGIN:{ //
+//////////////////
 static inline void Read(long &dest, std::string const &src) {
     if (src.size() < 1) {
         dest = -999999;
@@ -77,6 +185,20 @@ static inline void Read(long &dest, std::string const &src) {
     }
 }
 
+static inline void Read(long &dest, char const *src) {
+    if (strlen(src) < 1) {
+        dest = -999999;
+    } else {
+        dest = std::stol(src);
+    }
+}
+////////////////
+// long END.} //
+////////////////
+
+///////////////////////////
+// unsigned long BEGIN:{ //
+///////////////////////////
 static inline void Read(unsigned long &dest, std::string const &src) {
     if (src.size() < 1) {
         dest = 0;
@@ -84,5 +206,16 @@ static inline void Read(unsigned long &dest, std::string const &src) {
         sscanf(src.c_str(), "%lu", &dest);
     }
 }
+
+static inline void Read(unsigned long &dest, char const *src) {
+    if (strlen(src) < 1) {
+        dest = 0;
+    } else {
+        sscanf(src, "%lu", &dest);
+    }
+}
+/////////////////////////
+// unsigned long END.} //
+/////////////////////////
 
 #endif
