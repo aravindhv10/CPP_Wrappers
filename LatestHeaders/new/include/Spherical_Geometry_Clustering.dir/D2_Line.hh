d0dd305b1b288ee2cf95302d26dc9ae26d0dc826
Aravind <aravindhv10@gmail.com>
2021-02-12 10:15:56 +0530

    Added a parameterization function

--- LatestHeaders/new/include/Spherical_Geometry_Clustering.dir/D2_Line.hh
+++ LatestHeaders/new/include/Spherical_Geometry_Clustering.dir/D2_Line.hh
@@ -2,15 +2,30 @@
 
 #define _MACRO_CLASS_NAME_ D2GPS_Line
 template <typename TF, typename TI> class _MACRO_CLASS_NAME_ {
+    /////////////////////////
+    // Definitions BEGIN:{ //
+    /////////////////////////
   public:
     using TYPE_FLOAT = TF;
     using TYPE_INT   = TI;
     using TYPE_SELF  = _MACRO_CLASS_NAME_<TYPE_FLOAT, TYPE_INT>;
     using TYPE_POINT = D2GPS_Coordinates<TYPE_FLOAT, TYPE_INT>;
+    ///////////////////////
+    // Definitions END.} //
+    ///////////////////////
 
+    //////////////////////////
+    // Data Members BEGIN:{ //
+    //////////////////////////
   public:
     TYPE_POINT P1, P2;
+    ////////////////////////
+    // Data Members END.} //
+    ////////////////////////
 
+    //////////////////////////////////////////
+    // Blind perpendicular distance BEGIN:{ //
+    //////////////////////////////////////////
   public:
     inline TYPE_POINT get_perpendicular(TYPE_POINT const in) const {
         TYPE_POINT const p1_p2 = {P2.latitude - P1.latitude,
@@ -39,7 +54,39 @@ template <typename TF, typename TI> class _MACRO_CLASS_NAME_ {
         TYPE_FLOAT const mag_perp = std::sqrt(mag_perp_2);
         return mag_perp;
     }
+    ////////////////////////////////////////
+    // Blind perpendicular distance END.} //
+    ////////////////////////////////////////
 
+    ///////////////////////////////////////////////////
+    // Parameter based prpendicular distance BEGIN:{ //
+    ///////////////////////////////////////////////////
+  public:
+    inline TYPE_FLOAT get_t (TYPE_POINT const in) const {
+        TYPE_POINT const ab = P2 - P1;
+        TYPE_POINT const ap = in - P1;
+        TYPE_FLOAT const T = (ap * ab) / ab.magnitude_2();
+        return T;
+    }
+
+    inline TYPE_FLOAT distance_t (TYPE_POINT const in) const {
+        TYPE_FLOAT const t = get_t(in);
+        TYPE_FLOAT ret;
+        if( (0<=t) && (t<=1) ) {
+            TYPE_POINT const intersect = (P1*(static_cast<TYPE_FLOAT>(1)-t)) + (P2*t) ;
+            ret = intersect.HaverSineDistance(in);
+        } else {
+            ret = CPPFileIO::mymin(P1.HaverSineDistance(in),P2.HaverSineDistance(in));
+        }
+        return ret;
+    }
+    /////////////////////////////////////////////////
+    // Parameter based prpendicular distance END.} //
+    /////////////////////////////////////////////////
+
+    //////////////////////////////////////
+    // Constructor & Destructor BEGIN:{ //
+    //////////////////////////////////////
   public:
     _MACRO_CLASS_NAME_() {}
 
@@ -47,5 +94,8 @@ template <typename TF, typename TI> class _MACRO_CLASS_NAME_ {
       : P1(p1), P2(p2) {}
 
     ~_MACRO_CLASS_NAME_() {}
+    ////////////////////////////////////
+    // Constructor & Destructor END.} //
+    ////////////////////////////////////
 };
 #undef _MACRO_CLASS_NAME_
