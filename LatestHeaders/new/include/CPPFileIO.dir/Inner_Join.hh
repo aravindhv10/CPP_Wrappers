d1d2cd61d070c19bc41d3e2302e8e9dc80aa5c7c
Aravind <aravindhv10@gmail.com>
2021-03-30 17:12:08 +0530

    Added correct efficient inner join

--- LatestHeaders/new/include/CPPFileIO.dir/Inner_Join.hh
+++ LatestHeaders/new/include/CPPFileIO.dir/Inner_Join.hh
@@ -40,7 +40,7 @@ inline void Unique_Inner_Join(std::string const filename1,
 }
 
 template <typename T1, typename T2, typename O>
-inline void Inner_Join(std::string const filename1, std::string const filename2,
+inline void Bad_Inner_Join(std::string const filename1, std::string const filename2,
                        std::string const outfilename) {
 
     FileArray<T1> R1(filename1);
@@ -59,6 +59,34 @@ inline void Inner_Join(std::string const filename1, std::string const filename2,
             auto const &element2 = R2(i2);
             char const  cmp      = tmp(element1, element2);
             if (cmp == 0) { W(tmp); }
+        }
+    }
+}
+
+template <typename T1, typename T2, typename O>
+inline void Inner_Join(std::string const filename1, std::string const filename2,
+                       std::string const outfilename) {
+
+    FileArray<T1> R1(filename1);
+    FileArray<T2> R2(filename2);
+    FileWriter<O> W(outfilename);
+
+    size_t const start1 = 0;
+    size_t const end1   = R1();
+    size_t const end2   = R2();
+    size_t start2 = 0;
+
+    for (size_t i1 = start1; i1 < end1; i1++) {
+        auto const &element1 = R1(i1);
+        char cmp = 0 ;
+        for ( size_t i2 = start2 ; (i2 < end2) && (cmp>=0) ; i2++ ) {
+            O           tmp;
+            auto const &element2 = R2(i2);
+            cmp = tmp(element1, element2);
+            if (cmp == 0) { W(tmp); }
+            else if (cmp == 1) {
+              start2 = i2;
+            }
         }
     }
 }
