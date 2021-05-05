aa2693759f5aabe897c92ec2dfd875fae77a2604
aravind <aravindhv10@gmail.com>
2021-05-05 13:07:00 +0530

    added optimizing compilers

---
+++ LatestHeaders/new/src/MyCC/src/gcc-cxx.cc
@@ -0,0 +1,14 @@
+#include "./include/CPPFileIO.hh"
+
+int main (int const argc, char const ** argv) {
+  std::vector <std::string> args;
+  args.push_back("/usr/bin/g++");
+  for(int i=1;i<argc;i++){
+    args.push_back(argv[i]);
+  }
+  args.push_back("-std=c++17");
+  args.push_back("-Ofast");
+  args.push_back("-mtune=native");
+  args.push_back("-march=native");
+  CPPFileIO::starter_self(args);
+}
---
+++ LatestHeaders/new/src/MyCC/src/gcc-omp-cc.cc
@@ -0,0 +1,14 @@
+#include "./include/CPPFileIO.hh"
+
+int main (int const argc, char const ** argv) {
+  std::vector <std::string> args;
+  args.push_back("/usr/bin/gcc");
+  for(int i=1;i<argc;i++){
+    args.push_back(argv[i]);
+  }
+  args.push_back("-Ofast");
+  args.push_back("-mtune=native");
+  args.push_back("-march=native");
+  args.push_back("-fopenmp");
+  CPPFileIO::starter_self(args);
+}
