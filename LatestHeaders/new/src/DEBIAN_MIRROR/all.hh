77a3256b665807462877b20eabf6897b6338dac1
aravind <aravindhv10@gmail.com>
2021-05-08 13:34:34 +0530

    fixed debian mirror scripts

--- LatestHeaders/new/src/DEBIAN_MIRROR/all.hh
+++ LatestHeaders/new/src/DEBIAN_MIRROR/all.hh
@@ -1,10 +1,103 @@
 #ifndef _HEADER_GUARD_all_
 #define _HEADER_GUARD_all_
 
-#include "./LatestHeaders/new/include/Read_Show_Functions.hh"
+#include "./include/Read_Show_Functions.hh"
+
+#define _MACRO_CLASS_NAME_ element
+class _MACRO_CLASS_NAME_ {
+  public:
+    using TYPE_SELF = _MACRO_CLASS_NAME_;
+    using str       = std::string;
+
+    str filename, sha256;
+
+    inline bool operator>(TYPE_SELF const &other) const {
+        return sha256 > other.sha256;
+    }
+
+    inline bool operator<(TYPE_SELF const &other) const {
+        return sha256 < other.sha256;
+    }
+
+    inline bool operator==(TYPE_SELF const &other) const {
+        return (sha256 == other.sha256);
+    }
+
+    inline bool operator!=(TYPE_SELF const &other) const {
+        return (sha256 != other.sha256);
+    }
+
+    _MACRO_CLASS_NAME_(str FILENAME, str SHA256)
+      : filename(FILENAME), sha256(SHA256) {}
+
+    _MACRO_CLASS_NAME_() {}
+    ~_MACRO_CLASS_NAME_() {}
+};
+using elements = std::vector<element>;
+#undef _MACRO_CLASS_NAME_
+
+#define _MACRO_CLASS_NAME_ config
+class _MACRO_CLASS_NAME_ {
+  public:
+    using str  = std::string;
+    using strs = std::vector<str>;
+
+  public:
+    strs base_urls;
+    strs versions;
+    strs components;
+    strs final_urls;
+
+  private:
+    inline void generate_names() {
+        final_urls.clear();
+        strs final_files;
+
+        final_files.push_back("Contents-amd64.gz");
+        final_files.push_back("Contents-i386.gz");
+        final_files.push_back("binary-amd64/Packages.gz");
+        final_files.push_back("binary-i386/Packages.gz");
+        final_files.push_back("binary-all/Packages.gz");
+        final_files.push_back("binary-amd64/Packages.xz");
+        final_files.push_back("binary-i386/Packages.xz");
+        final_files.push_back("binary-all/Packages.xz");
+        final_files.push_back("binary-amd64/Packages");
+        final_files.push_back("binary-i386/Packages");
+        final_files.push_back("binary-all/Packages");
+
+        for (size_t i = 0; i < base_urls.size(); i++) {
+            for (size_t j = 0; j < versions.size(); j++) {
+                for (size_t k = 0; k < components.size(); k++) {
+                    for (size_t l = 0; l < final_files.size(); l++) {
+                        str tmp = base_urls[i] + "/dists/" + versions[j] + "/" +
+                                  components[k] + "/" + final_files[l];
+                        final_urls.push_back(tmp);
+                    }
+                }
+            }
+        }
+    }
+
+  public:
+    inline strs &operator()() {
+        generate_names();
+        return final_urls;
+    }
+
+    _MACRO_CLASS_NAME_() {
+        base_urls.push_back("http://ftp.debian.org/debian");
+
+        versions.push_back("stable-backports");
+
+        components.push_back("main");
+        components.push_back("contrib");
+        components.push_back("non-free");
+    }
+    ~_MACRO_CLASS_NAME_() {}
+};
+#undef _MACRO_CLASS_NAME_
 
 #define _MACRO_CLASS_NAME_ MainWorker
-
 class _MACRO_CLASS_NAME_ {
   public:
     using string      = std::string;
@@ -68,14 +161,18 @@ class _MACRO_CLASS_NAME_ {
     }
 
   private:
-    strings list_sha, list_filename;
+    elements store;
 
   private:
     static inline file_buffer get_file(string const in) {
-        CPPFileIO::FileArray<char> reader(in);
-        file_buffer                ret(reader() + 1);
-        reader(0, ret);
-        ret(ret() - 1) = 0;
+        CPPFileIO::FileArray<char const> reader(in);
+        size_t const                     limit = reader();
+        file_buffer                      ret(limit + 1);
+        memcpy(
+          /* void *dest = */ static_cast<void *>(&(ret(0))),
+          /* const void *src = */ static_cast<const void *>(&(reader(0))),
+          /* size_t n = */ static_cast<size_t>(limit));
+        ret(limit) = 0;
         return ret;
     }
 
@@ -89,15 +186,15 @@ class _MACRO_CLASS_NAME_ {
     }
 
     static inline void WGET_MIRROR(string const in) {
-        CPPFileIO::ExternalStarter<true>::GET("/usr/bin/wget")("-c")(
-          "-m")("-np")(in);
+        CPPFileIO::ExternalStarter<true>::GET("/usr/bin/wget")("-c")("-m")(
+          "--timeout=10")("-np")(in);
     }
 
     static inline void WGET(string const in, string const out) {
         bool constexpr use_wget = false;
         if (use_wget) {
-            CPPFileIO::ExternalStarter<true>::GET("/usr/bin/wget")("-c")(
-              in)("-O")(out);
+            CPPFileIO::ExternalStarter<true>::GET("/usr/bin/wget")("-c")(in)(
+              "-O")(out);
             CPPFileIO::ExternalStarter<true>::GET("/usr/bin/aria2c")("-x16")(
               "-c")(in);
         } else {
@@ -146,25 +243,50 @@ class _MACRO_CLASS_NAME_ {
     }
 
     inline bool is_dbg(size_t const i) const {
-        return is_dbg(list_filename[i]);
+        return is_dbg(store[i].filename);
     }
 
     inline void get_list() {
-        list_sha.clear();
-        list_filename.clear();
+        store.clear();
 
-        strings in;
-        get_package_list(in);
+        elements tmpbuf; /* populate the tmp buffer: */
+        {
+            strings in;
+            get_package_list(in);
+            for (size_t id_in = 0; id_in < in.size(); id_in++) {
 
-        for (size_t id_in = 0; id_in < in.size(); id_in++) {
-            strings lines;
-            get_lines(lines, in[id_in]);
-            for (size_t i = 0; i < lines.size(); i++) {
-                if (shamatch(lines[i])) { list_sha.push_back(&(lines[i][8])); }
+                strings lines;
+                get_lines(lines, in[id_in]);
 
-                if (filenamematch(lines[i])) {
-                    list_filename.push_back(&(lines[i][10]));
+                strings list_sha, list_filename;
+                for (size_t i = 0; i < lines.size(); i++) {
+                    if (shamatch(lines[i])) {
+                        list_sha.push_back(&(lines[i][8]));
+                    }
+
+                    if (filenamematch(lines[i])) {
+                        list_filename.push_back(&(lines[i][10]));
+                    }
                 }
+
+                size_t const min =
+                  CPPFileIO::mymin(list_sha.size(), list_filename.size());
+
+                for (size_t i = 0; i < min; i++) {
+                    element tmp;
+                    tmp.filename = list_filename[i];
+                    tmp.sha256   = list_sha[i];
+                    tmpbuf.push_back(tmp);
+                }
+            }
+        }
+
+        if (tmpbuf.size() > 1) { std::sort(tmpbuf.begin(), tmpbuf.end()); }
+
+        if (tmpbuf.size() > 0) {
+            store.push_back(tmpbuf[0]);
+            for (size_t i = 1; i < tmpbuf.size(); i++) {
+                if (tmpbuf[i] != tmpbuf[i - 1]) { store.push_back(tmpbuf[i]); }
             }
         }
     }
@@ -190,27 +312,27 @@ class _MACRO_CLASS_NAME_ {
     inline void clean_sha() {
         get_list();
         {
-            CPPFileIO::ExternalStarter<true>::GET("/bin/mv")("-vf")(
-              "--")("./SHA256")("WASTE");
+            CPPFileIO::ExternalStarter<true>::GET("/bin/mv")("-vf")("--")(
+              "./SHA256")("WASTE");
         }
         mkdir("./SHA256", 0755);
-        for (size_t i = 0; i < list_sha.size(); i++) {
+        for (size_t i = 0; i < store.size(); i++) {
             if (!is_dbg(i)) {
-                string const input = "./WASTE/" + list_sha[i];
-                CPPFileIO::ExternalStarter<true>::GET("/bin/mv")("-vf")(
-                  "--")(input)("./SHA256");
+                string const input = "./WASTE/" + store[i].sha256;
+                CPPFileIO::ExternalStarter<true>::GET("/bin/mv")("-vf")("--")(
+                  input)("./SHA256");
             }
         }
         {
-            CPPFileIO::ExternalStarter<true>::GET("/bin/rmdir")("-v")(
-              "--")("WASTE");
+            CPPFileIO::ExternalStarter<true>::GET("/bin/rmdir")("-v")("--")(
+              "WASTE");
         }
     }
 
     inline void show_dbg() {
         get_list();
-        for (size_t i = 0; i < list_filename.size(); i++) {
-            if (is_dbg(i)) { printf("DEBUG: %s\n", list_filename[i].c_str()); }
+        for (size_t i = 0; i < store.size(); i++) {
+            if (is_dbg(i)) { printf("DEBUG: %s\n", store[i].filename.c_str()); }
         }
     }
 
@@ -227,34 +349,37 @@ class _MACRO_CLASS_NAME_ {
             size_t index;
         MainLoop:
             index = counter++;
-            if (index < list_sha.size()) {
+            if (index < store.size()) {
                 if (!is_dbg(index)) {
-                    WGET(base_url + list_filename[index],
-                         out_dir + list_sha[index]);
+                    WGET(base_url + store[index].filename,
+                         out_dir + store[index].sha256);
                 }
                 goto MainLoop;
             }
         }
     }
 
-    inline void setup_files () {
+    inline void setup_files() {
         get_list();
         string const prefix = get_prefix();
 #pragma omp parallel for
-        for(size_t i=0;i< list_filename.size();i++ ){
-            if(!is_dbg(i)){
-                string const outfile = prefix + list_filename[i];
-                size_t counts = 0;
-                for(char const & i : outfile) {
-                    if(i=='/'){counts++;}
+        for (size_t i = 0; i < store.size(); i++) {
+            if (!is_dbg(i)) {
+                string const outfile = prefix + store[i].filename;
+                size_t       counts  = 0;
+                for (char const &i : outfile) {
+                    if (i == '/') { counts++; }
                 }
-                if(counts>0){counts--;}
-                string infile = "./SHA256/" + list_sha[i];
-                while(counts>0){
-                  infile = "../" + infile ;
-                  counts--;
+                if (counts > 0) { counts--; }
+                string infile = "./SHA256/" + store[i].sha256;
+                while (counts > 0) {
+                    infile = "../" + infile;
+                    counts--;
                 }
-                {CPPFileIO::ExternalStarter<true>::GET("./do_link")(outfile)(infile);}
+                {
+                    CPPFileIO::ExternalStarter<true>::GET("./do_link")(outfile)(
+                      infile);
+                }
             }
         }
     }
@@ -263,7 +388,6 @@ class _MACRO_CLASS_NAME_ {
     _MACRO_CLASS_NAME_() {}
     ~_MACRO_CLASS_NAME_() {}
 };
-
 #undef _MACRO_CLASS_NAME_
 
 #endif
