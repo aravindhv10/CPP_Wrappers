75d23aacc2b7575a6b3061f9436285a25178a4fb
aravind <aravindhv10@gmail.com>
2021-05-10 10:47:31 +0530

    Included a template for launcher

---
+++ LatestHeaders/new/src/MyCC/src/launcher.cc
@@ -0,0 +1,11 @@
+#include "./include/CPPFileIO.hh"
+
+int main (int const argc, char const ** argv) {
+    std::vector<std::string> args;
+    args.push_back("/lib64/ld-linux-x86-64.so.2");
+    args.push_back("--library-path");
+    args.push_back(".");
+    args.push_back("./exe");
+    for (int i = 1; i < argc; i++) { args.push_back(argv[i]); }
+    CPPFileIO::starter_self(args);
+}
